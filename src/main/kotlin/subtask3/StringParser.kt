81efe526893add3bf73e9daeaf84192fc8cc4d37
AlesyaZ <60661142+AlesyaZ@users.noreply.github.com>
2021-05-29 21:17:07 +0300

    add: solution StringParser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,26 @@
 package subtask3
 
 class StringParser {
-
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var resultStringParser = mutableListOf<String>()
+        val changeInputString = StringBuilder(inputString)
+        val bracketsOpen = arrayOf('[','<','(')
+        val bracketsClose = arrayOf(']','>',')')
+        var count = 0
+
+        for (i in inputString.lastIndex downTo 1) {
+            if (inputString[i] in bracketsOpen) {
+                for (ind in i..inputString.lastIndex) {
+                    if (changeInputString[ind] in bracketsClose &&
+                        bracketsOpen.indexOf(inputString[i]) == bracketsClose.indexOf(inputString[ind])) {
+                        count = ind
+                        changeInputString[ind] = ' '
+                        break
+                    }
+                }
+                resultStringParser.add(0, inputString.substring(i + 1, count))
+            }
+        }
+        return resultStringParser.toTypedArray()
     }
 }
